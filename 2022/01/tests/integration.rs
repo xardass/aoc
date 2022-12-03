0136c1b597ad1d03b413ec8c66b978169d20d475
Xavier Hendrickx <xavier.hendrickx@ngrave.io>
2022-12-02 20:26:30 -0800

    fmt fix

--- 2022/01/tests/integration.rs
+++ 2022/01/tests/integration.rs
@@ -3,7 +3,7 @@ use aoc_2022_01::{part1, part2};
 #[test]
 fn test_part1() {
     let input = include_str!("../input/input.txt");
-    assert_eq!(part1(input), 65912);
+    assert_eq!(part1(input), 69883);
 }
 
 #[test]
